4893502b517e64eec0af190afcafc54cd6cc4a66
emguse <30695541+emguse@users.noreply.github.com>
2022-04-03 19:30:34 +0900

    add new file

---
+++ offsetcircule.py
@@ -0,0 +1,18 @@
+from turtle import color
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = 200
+b = 300
+
+r = 300
+x = np.arange(a - r, a + r + 1)
+
+y = np.sqrt(r ** 2 - (x - a) ** 2) + b
+y2 = -y + 2 * b
+
+plt.plot(x, y)
+plt.plot(x, y2)
+plt.axis('equal')
+plt.grid(color='0.8')
+plt.show()
