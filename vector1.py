05f8193ae4e23fc10b0d16e73e95383394800e96
emguse <30695541+emguse@users.noreply.github.com>
2022-04-04 21:19:54 +0900

    add new file

---
+++ vector1.py
@@ -0,0 +1,9 @@
+import math
+
+x = 3
+y = 2
+
+rad = math.atan2(x, y)
+th = math.degrees(rad)
+
+print(th)
---
+++ vector2.py
@@ -0,0 +1,8 @@
+import math
+
+x = 2
+y = 3
+
+oa = math.sqrt(x ** 2 + y ** 2)
+
+print(oa)
