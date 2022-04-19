179d8d8a938462c32ae98ca75d4388d2c6461f0e
emguse <30695541+emguse@users.noreply.github.com>
2022-04-19 21:11:31 +0900

    add new file

--- matrix10.py
+++ matrix10.py
@@ -17,3 +17,22 @@ plt.plot(p2[0, :], p2[1, :])
 plt.axis('equal')
 plt.grid(color='0.8')
 plt.show()
+
+
+
+
+p = np.matrix([[1, 3, 3, 1], [1, 1, 2, 1]])
+
+a = np.matrix(([[-1, 0], [0, 1]]))
+
+p2 = a * p
+
+print(p2)
+
+p = np.array(p)
+p2 = np.array(p2)
+plt.plot(p[0, :], p[1, :])
+plt.plot(p2[0, :], p2[1, :])
+plt.axis('equal')
+plt.grid(color='0.8')
+plt.show()
